ff3705c934b16cb6c5207c8fabb190b1eff712c4
MalkovaLiliya <lilok-ny@yandex.ru>
2021-03-12 19:16:01 +0300

    Чат с приватными сообщ

--- src/server/ClientHandler.java
+++ src/server/ClientHandler.java
@@ -10,6 +10,7 @@ public class ClientHandler {
     private Socket socket;
     private DataInputStream in;
     private DataOutputStream out;
+    private String nickName;
 
     public ClientHandler(Server server, Socket socket) {
 
@@ -19,43 +20,75 @@ public class ClientHandler {
             in = new DataInputStream(socket.getInputStream());
             out = new DataOutputStream(socket.getOutputStream());
 
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        while (true) {
-                            String str = in.readUTF();
-
-
-                            if (str.equals("/end")) {
-                                System.out.println("Клиент отключился");
+            new Thread(() -> {
+                try {
+                    // цикл аутентифиукаии
+                    while (true) {
+                        String str = in.readUTF();
+                        if (str.startsWith("/auth")) {
+                            String[] token = str.split("\\s");
+                            String newNick = server.getAuthService()
+                                    .getNickByLoginAndPassword(token[1], token[2]);
+                            if (newNick != null) {
+                                nickName = newNick;
+                                sendMsg("/authok " + nickName);
+                                server.subscribe(this);
+                                System.out.println("Клиент " + nickName + " подключился");
                                 break;
+                            } else {
+                                sendMsg("Неверный логин / пароль");
                             }
-                            server.broadcastMsg(str);
                         }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    } finally {
-                        System.out.println("Клиент отключился");
-                        try {
-                            socket.close();
-                        } catch (IOException ioException) {
-                            ioException.printStackTrace();
-                        }
+                    }
 
+                    //цикл работы
+                    while (true) {
+                        String str = in.readUTF();
+                        if (str.equals("/end")) {
+                            break;
+                        }
+                        else if (str.startsWith("/w")) {
+                            String[] parts = str.split("\\s", 3);
+                            if (parts.length != 3) {
+                                sendMsg("Ошибка в команде");
+                                continue;
+                            }
+                            if (!server.sendPrivateMsg(this,parts[1],parts[2])){
+                                sendMsg("Ошибка при отправке личного сообщения");
+                            }
+                        }
+//                            System.out.println("Клиент " + str);
+//                            out.writeUTF("echo: " + str);
+                        else
+                        server.broadcastMsg(this, str);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    System.out.println("Клиент отключился");
+                    server.unsubscribe(this);
+                    try {
+                        socket.close();
+                    } catch (IOException ioException) {
+                        ioException.printStackTrace();
                     }
                 }
             }).start();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 
-    void sendMessage(String msg){
+    void sendMsg(String msg) {
         try {
             out.writeUTF(msg);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public String getNickName(){
+        return nickName;
     }
 }
